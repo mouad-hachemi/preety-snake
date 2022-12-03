ca73d09c2e5f277a78ed27ba068e64f0437980a4
Mouad Hachemi <mouad.hachemi2k@gmail.com>
2022-12-03 19:47:42 +0100

    Added boundaries logic and refined some code

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # Simple snake game made with Pygame library.
-import pygame
-import random
+import sys, pygame, random
 
 # Colors
 WHITE = (255, 255, 255)
-GRAY = pygame.Color('#cad2c5')
-WARM_BLUE = pygame.Color('#1e6091')
+GRAY = pygame.Color("#cad2c5")
+WARM_BLUE = pygame.Color("#1e6091")
 
 # Screen Dimensions.
 WIDTH = 992
@@ -19,10 +18,18 @@ GRID_SIZE = 32
 NUM_OF_GRIDS_X = 30
 NUM_OF_GRIDS_Y = 14
 
+BOUNDARIES = {
+    "LeftX": GRID_SIZE,
+    "RightX": WIDTH - GRID_SIZE,
+    "TopY": GRID_SIZE,
+    "BottomY": HEIGHT - GRID_SIZE,
+}
+
 # Initializing pygame, and creating game window.
 pygame.init()
 pygame.mixer.init()
 clock = pygame.time.Clock()
+
 
 class Snake(pygame.sprite.Sprite):
     def __init__(self):
@@ -30,16 +37,25 @@ class Snake(pygame.sprite.Sprite):
         self.image = pygame.Surface((GRID_SIZE, GRID_SIZE))
         self.image.fill(WARM_BLUE)
         self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(GRID_SIZE, WIDTH - 3 * GRID_SIZE, GRID_SIZE)
-        self.rect.y = random.randrange(GRID_SIZE, HEIGHT - 3 * GRID_SIZE, GRID_SIZE)
+        self.rect.x = random.randrange(
+            BOUNDARIES["LeftX"] + GRID_SIZE * 2,
+            BOUNDARIES["RightX"] - GRID_SIZE * 2,
+            GRID_SIZE,
+        )
+        self.rect.y = random.randrange(
+            BOUNDARIES["TopY"] + GRID_SIZE * 2,
+            BOUNDARIES["BottomY"] - GRID_SIZE * 2,
+            GRID_SIZE,
+        )
         self.vel = (0, 0)
-    
+
     def update(self):
-        self.rect.x += self.vel[0] * GRID_SIZE # The Xth value of vel.
-        self.rect.y += self.vel[1] * GRID_SIZE # The Yth value of vel.
+        self.rect.x += self.vel[0] * GRID_SIZE  # The Xth value of vel.
+        self.rect.y += self.vel[1] * GRID_SIZE  # The Yth value of vel.
+
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Pretty Snake!')
+pygame.display.set_caption("Pretty Snake!")
 
 all_sprites = pygame.sprite.Group()
 
@@ -47,37 +63,62 @@ snake = Snake()
 
 all_sprites.add(snake)
 
+
+def quit_game():
+    pygame.quit()
+    sys.exit()
+
+
 def draw_grid():
     for i in range(1, NUM_OF_GRIDS_X + 1):
-            pygame.draw.line(screen, GRAY, (i * GRID_SIZE, GRID_SIZE), (i * GRID_SIZE, HEIGHT - GRID_SIZE))
+        pygame.draw.line(
+            screen,
+            GRAY,
+            (i * GRID_SIZE, BOUNDARIES["TopY"]),
+            (i * GRID_SIZE, BOUNDARIES["BottomY"]),
+        )
     for j in range(1, NUM_OF_GRIDS_Y + 1):
-        pygame.draw.line(screen, GRAY, (GRID_SIZE, j * GRID_SIZE), (WIDTH - GRID_SIZE, j * GRID_SIZE))
+        pygame.draw.line(
+            screen,
+            GRAY,
+            (BOUNDARIES["LeftX"], j * GRID_SIZE),
+            (BOUNDARIES["RightX"], j * GRID_SIZE),
+        )
+
+
+def is_out(player):
+    if player.x < BOUNDARIES["LeftX"] or player.x >= BOUNDARIES["RightX"]:
+        return True
+    elif player.y < BOUNDARIES["TopY"] or player.y >= BOUNDARIES["BottomY"]:
+        return True
+    return False
+
 
 def main():
-    running = True
-    while running:
+    while True:
         clock.tick(FPS)
         # Process Input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                quit_game()
             elif event.type == pygame.KEYDOWN:
                 snake.vel = {
                     pygame.K_UP: (0, -1),
                     pygame.K_RIGHT: (1, 0),
                     pygame.K_DOWN: (0, 1),
-                    pygame.K_LEFT: (-1, 0)
+                    pygame.K_LEFT: (-1, 0),
                 }.get(event.key, (0, 0))
 
         # Update
         all_sprites.update()
+        if is_out(snake.rect):
+            quit_game()
         # Draw
         screen.fill(WHITE)
         draw_grid()
         all_sprites.draw(screen)
         pygame.display.flip()
-    pygame.quit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
