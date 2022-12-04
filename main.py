f5d682946a35d043e193dcbad644e18d0fa8025e
Mouad Hachemi <mouad.hachemi2k@gmail.com>
2022-12-04 10:38:42 +0100

    Implemented snake body

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import sys, pygame, random
 # Colors
 WHITE = (255, 255, 255)
 GRAY = pygame.Color("#cad2c5")
-WARM_BLUE = pygame.Color("#1e6091")
+HEAD_COLOR = pygame.Color("#168AAD")
+BODY_COLOR = pygame.Color("#34A0A4")
+FRUIT_COLOR = pygame.Color("#76C893")
 OCEAN_GREAN = pygame.Color("#52B69A")
 
 # Screen Dimensions.
@@ -16,8 +18,8 @@ FPS = 8
 
 # GRID Params.
 GRID_SIZE = 32
-NUM_OF_GRIDS_X = 30
-NUM_OF_GRIDS_Y = 14
+NUM_OF_GRIDS_X = 31
+NUM_OF_GRIDS_Y = 15
 
 BOUNDARIES = {
     "LeftX": GRID_SIZE,
@@ -32,53 +34,28 @@ pygame.mixer.init()
 clock = pygame.time.Clock()
 
 
-class Snake(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface((GRID_SIZE, GRID_SIZE))
-        self.image.fill(WARM_BLUE)
-        self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(
-            BOUNDARIES["LeftX"] + GRID_SIZE * 2,
-            BOUNDARIES["RightX"] - GRID_SIZE * 2,
-            GRID_SIZE,
-        )
-        self.rect.y = random.randrange(
-            BOUNDARIES["TopY"] + GRID_SIZE * 2,
-            BOUNDARIES["BottomY"] - GRID_SIZE * 2,
-            GRID_SIZE,
-        )
-        self.vel = (0, 0)
-
-    def update(self):
-        self.rect.x += self.vel[0] * GRID_SIZE  # The Xth value of vel.
-        self.rect.y += self.vel[1] * GRID_SIZE  # The Yth value of vel.
+def spawn_head():
+    x = random.randrange(
+        BOUNDARIES["LeftX"] + GRID_SIZE * 2,
+        BOUNDARIES["RightX"] - GRID_SIZE * 2,
+        GRID_SIZE,
+    )
+    y = random.randrange(
+        BOUNDARIES["TopY"] + GRID_SIZE * 2,
+        BOUNDARIES["BottomY"] - GRID_SIZE * 2,
+        GRID_SIZE,
+    )
+    return x, y
 
 
-class Fruit(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface((GRID_SIZE, GRID_SIZE))
-        self.image.fill(OCEAN_GREAN)
-        self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(
-            BOUNDARIES["LeftX"], BOUNDARIES["RightX"] - GRID_SIZE, GRID_SIZE
-        )
-        self.rect.y = random.randrange(
-            BOUNDARIES["TopY"], BOUNDARIES["BottomY"] - GRID_SIZE, GRID_SIZE
-        )
-
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Pretty Snake!")
-
-all_sprites = pygame.sprite.Group()
-
-snake = Snake()
-fruit = Fruit()
-
-all_sprites.add(fruit)
-all_sprites.add(snake)
+def spawn_fruit():
+    x = random.randrange(
+        BOUNDARIES["LeftX"], BOUNDARIES["RightX"] - GRID_SIZE, GRID_SIZE
+    )
+    y = random.randrange(
+        BOUNDARIES["TopY"], BOUNDARIES["BottomY"] - GRID_SIZE, GRID_SIZE
+    )
+    return x, y
 
 
 def quit_game():
@@ -87,14 +64,14 @@ def quit_game():
 
 
 def draw_grid():
-    for i in range(1, NUM_OF_GRIDS_X + 1):
+    for i in range(1, NUM_OF_GRIDS_X):
         pygame.draw.line(
             screen,
             GRAY,
             (i * GRID_SIZE, BOUNDARIES["TopY"]),
             (i * GRID_SIZE, BOUNDARIES["BottomY"]),
         )
-    for j in range(1, NUM_OF_GRIDS_Y + 1):
+    for j in range(1, NUM_OF_GRIDS_Y):
         pygame.draw.line(
             screen,
             GRAY,
@@ -103,12 +80,83 @@ def draw_grid():
         )
 
 
-def is_out(player):
-    if player.x < BOUNDARIES["LeftX"] or player.x >= BOUNDARIES["RightX"]:
+def is_out(head):
+    head_coor = head.rect
+    if head_coor.x < BOUNDARIES["LeftX"] or head_coor.x >= BOUNDARIES["RightX"]:
         return True
-    elif player.y < BOUNDARIES["TopY"] or player.y >= BOUNDARIES["BottomY"]:
+    elif head_coor.y < BOUNDARIES["TopY"] or head_coor.y >= BOUNDARIES["BottomY"]:
         return True
     return False
+
+
+class Snake:
+    def __init__(self):
+        super().__init__()
+        self.body = []
+        self.head = None
+        self.vel = (0, 0)
+
+    def draw(self, surface):
+        for i, block in enumerate(self.body):
+            if i == 0:
+                block.image.fill(HEAD_COLOR)
+            else:
+                block.image.fill(BODY_COLOR)
+            surface.blit(block.image, block.rect)
+
+    def add_block(self, block):
+        self.head = block
+        self.body = [block] + self.body
+
+    def update(self):
+        x, y = self.head.rect.topleft
+        x += self.vel[0] * GRID_SIZE  # The Xth value of vel.
+        y += self.vel[1] * GRID_SIZE  # The Yth value of vel.
+        new_head = Block((x, y))
+        self.add_block(new_head)
+        self.body.pop()
+        self.eat()
+
+    def eat(self):
+        if self.head.rect == fruit.rect:
+            x, y = fruit.rect.topleft
+            x += self.vel[0] * GRID_SIZE
+            y += self.vel[1] * GRID_SIZE
+            block = Block((x, y))
+            self.add_block(block)
+
+
+class Block:
+    def __init__(self, cor):
+        super().__init__()
+        self.image = pygame.Surface((GRID_SIZE, GRID_SIZE))
+        self.rect = self.image.get_rect()
+        self.rect.x = cor[0]
+        self.rect.y = cor[1]
+
+
+class Fruit:
+    def __init__(self, cor):
+        super().__init__()
+        self.image = pygame.Surface((GRID_SIZE, GRID_SIZE))
+        self.image.fill(FRUIT_COLOR)
+        self.rect = self.image.get_rect()
+        self.rect.x = cor[0]
+        self.rect.y = cor[1]
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pretty Snake!")
+
+snake = Snake()
+
+head = Block(spawn_head())
+fruit = Fruit(spawn_fruit())
+
+snake.add_block(head)
 
 
 def main():
@@ -127,13 +175,14 @@ def main():
                 }.get(event.key, (0, 0))
 
         # Update
-        all_sprites.update()
-        if is_out(snake.rect):
+        snake.update()
+        if is_out(snake.head):
             quit_game()
         # Draw
         screen.fill(WHITE)
         draw_grid()
-        all_sprites.draw(screen)
+        fruit.draw(screen)
+        snake.draw(screen)
         pygame.display.flip()
 
 
